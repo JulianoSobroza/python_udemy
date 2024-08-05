f2e4165c243ebca45d6dc6f16a2df4269bd19e42
JulianoSobroza <sobrozajuliano@gmail.com>
2024-08-04 22:45:47 -0300

    mais uns 2

--- S9_ola_django/home/views.py
+++ S9_ola_django/home/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-
-#from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     print('home')
-    return HttpResponse('Página Inicial de home em /home/views.py')
+    
+    return render(
+        request,
+        'home.html'
+    )
